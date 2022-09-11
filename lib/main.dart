e9c48d6969874bca578991228eb5158abadddc16
amrdesokyy <moravvv@gmail.com>
2022-09-11 23:43:05 +0200

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key); //test
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       //**
